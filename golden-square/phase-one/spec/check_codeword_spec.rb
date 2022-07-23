c63a25159ff86c5b79adb3e99d19d5f7d517d94e
zb-coder <zainab_baruud@hotmail.co.uk>
2022-07-23 18:08:01 +0100

    testing check_codeword method

---
+++ golden-square/phase-one/spec/check_codeword_spec.rb
@@ -0,0 +1,18 @@
+require 'check_codeword'
+
+describe 'check codeword method' do
+    it 'returns the correct message is given the correct codeword' do
+        result = check_codeword("horse")
+        expect(result).to eq "Correct! Come in."
+    end
+
+    it 'returns the close message if given a close codeword' do
+        result = check_codeword("he")
+        expect(result).to eq "Close, but nope."
+    end
+
+    it "returns the wrong message if given the wrong codeword" do
+        result = check_codeword("no")
+        expect(result).to eq "WRONG!"
+    end
+end
