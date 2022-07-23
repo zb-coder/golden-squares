d8353f202388a9aa0f36bf255d12d2186aecc99c
zb-coder <zainab_baruud@hotmail.co.uk>
2022-07-23 19:54:37 +0100

    adding a comment

--- golden-square/phase-one/02-testing-classes/spec/reminder_spec.rb
+++ golden-square/phase-one/02-testing-classes/spec/reminder_spec.rb
@@ -8,3 +8,7 @@ describe Reminder do
         expect(result).to eq "Walk the dog, Kay!"
     end
 end
+
+# Our example involves calling a number of 
+# methods that exercise the intended behaviour 
+# of the class.
