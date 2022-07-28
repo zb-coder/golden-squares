d541ec683337e0cf34d9dea4835e84ff318ce917
zb-coder <zainab_baruud@hotmail.co.uk>
2022-07-28 12:10:48 +0100

    refacturing tests: adding context blocks

--- golden-square/phase-two/spec/make_snippet_spec.rb
+++ golden-square/phase-two/spec/make_snippet_spec.rb
@@ -1,18 +1,24 @@
 require 'make_snippet'
 
 describe "make_snippet method" do
-    it 'returns an empty string' do
-        result = make_snippet("")
-        expect(result).to eq("")
+    context "given an empty string" do
+        it 'returns an empty string' do
+            result = make_snippet("")
+            expect(result).to eq("")
+        end
     end
 
-    it 'returns a string of four words' do
-        result = make_snippet("his name is dave")
-        expect(result).to eq("his name is dave")
+    context "given a string of four words" do
+        it 'returns that string' do
+            result = make_snippet("his name is dave")
+            expect(result).to eq("his name is dave")
+        end
     end
 
-    it 'returns a string of five words with a ... when given 6 words' do
-        result = make_snippet("his name is dave james green")
-        expect(result).to eq("his name is dave james...")
+    context "given a string of six words" do
+        it 'returns a string of five words with a ...' do
+            result = make_snippet("his name is dave james green")
+            expect(result).to eq("his name is dave james...")
+        end
     end
 end
