ffb6b53481fe4ed35c220a7eb5cd2d39a0631e30
zb-coder <zainab_baruud@hotmail.co.uk>
2022-08-02 17:01:58 +0100

    Changing code

--- golden-square/phase-two/lib/calculate_reading_time.rb
+++ golden-square/phase-two/lib/calculate_reading_time.rb
@@ -1,11 +1,4 @@
 def calculate_reading_time(text)
     words = text.split(" ")
- 
-    if words.length == 0
-        return 0
-    elsif words.length <= 200
-        return 1
-    elsif words.length <= 400
-        return 2
-    end
+    return (words.length / 200.to_f).ceil()
 end
