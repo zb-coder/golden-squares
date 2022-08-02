4c40bdb65fb48644f0e1c15d6b7c45e96d16ecb5
zb-coder <zainab_baruud@hotmail.co.uk>
2022-08-02 17:04:51 +0100

    Adding final test for calculate reading time method

--- golden-square/phase-two/spec/calculate_reading_time_spec.rb
+++ golden-square/phase-two/spec/calculate_reading_time_spec.rb
@@ -27,5 +27,12 @@ describe 'calculate_reading_time method' do
             result = calculate_reading_time("one " * 300)
             expect(result).to eq 2
         end
+    end
+    
+    context 'given a text of five hundred words' do
+        it 'returns three' do
+            result = calculate_reading_time("one " * 500)
+            expect(result).to eq 3
+        end
     end 
 end
