8d2706a89380b6fc3b1abaa451b82ced2d07c4af
zb-coder <zainab_baruud@hotmail.co.uk>
2022-08-04 10:35:03 +0100

    Adding a test to return true when a string includes #TODO and making it pass

--- golden-square/phase-two/lib/contains_todo.rb
+++ golden-square/phase-two/lib/contains_todo.rb
@@ -1,4 +1,10 @@
 def contains_todo(text)
     
    fail "Not a task" if text.empty?
+
+   if text.include?('#TODO')
+      return true
+   else
+      return false
+   end
 end
--- golden-square/phase-two/spec/contains_todo_spec.rb
+++ golden-square/phase-two/spec/contains_todo_spec.rb
@@ -6,4 +6,10 @@ describe 'contains_todo method' do
             expect{ contains_todo("") }.to raise_error "Not a task"
         end
     end
+
+    context 'given a string that contains #TODO' do
+        it 'returns true' do
+            expect(contains_todo("#TODO")).to eq true
+        end
+    end
 end
