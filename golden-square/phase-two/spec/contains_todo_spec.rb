3f221bf5343c4e3c939348435742cae651876834
zb-coder <zainab_baruud@hotmail.co.uk>
2022-08-04 11:20:34 +0100

    Adding tests that return false

--- golden-square/phase-two/spec/contains_todo_spec.rb
+++ golden-square/phase-two/spec/contains_todo_spec.rb
@@ -12,4 +12,28 @@ describe 'contains_todo method' do
             expect(contains_todo("#TODO")).to eq true
         end
     end
+
+    context 'given a string that contains TODO instead of #TODO' do
+        it 'returns false' do
+            expect(contains_todo("TODO")).to eq false
+        end
+    end
+
+    context 'given a string that contains # instead of #TODO' do
+        it 'returns false' do
+            expect(contains_todo("#")).to eq false
+        end
+    end
+
+    context 'given a string that contains # todo instead of #TODO' do
+        it 'returns false' do
+            expect(contains_todo("# todo")).to eq false
+        end
+    end
+
+    context 'given a string that contains # TODO instead of #TODO' do
+        it 'returns false' do
+            expect(contains_todo("# TODO")).to eq false
+        end
+    end
 end
