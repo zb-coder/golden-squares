23e99256eb60a8dda120d6c3b7b49535252349bb
zb-coder <zainab_baruud@hotmail.co.uk>
2022-08-05 11:31:53 +0100

    managed to debug the encode method

--- golden-square/phase-two/debugging/exercise_2.rb
+++ golden-square/phase-two/debugging/exercise_2.rb
@@ -1,19 +1,31 @@
 def encode(plaintext, key)
-    cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+    cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+    # p "key": key
+    # p "key.chars": key.chars
+    # p "key.chars.uniq": key.chars.uniq
+    # p "key.chars.uniq + (('a'...'z').to_a": key.chars.uniq + (('a'...'z').to_a
+    # exit
     ciphertext_chars = plaintext.chars.map do |char|
-      (65 + cipher.find_index(char)).chr
+        # p "cipher.find_index(char)"
+        # p cipher.find_index(char)
+        # p char
+        # p cipher
+    (65 + cipher.find_index(char)).chr
     end
     return ciphertext_chars.join
   end
   
   def decode(ciphertext, key)
-    cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+    cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
     plaintext_chars = ciphertext.chars.map do |char|
       cipher[65 - char.ord]
     end
     return plaintext_chars.join
   end
   
+
+  p encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+  p decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
   # Intended output:
   #
   # > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
