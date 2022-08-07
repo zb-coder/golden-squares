e5ea92255154d5450ccb9951b30a9d3abc336746
zb-coder <zainab_baruud@hotmail.co.uk>
2022-08-07 11:52:04 +0100

    able to add a title and contents to the diaryentry

--- golden-square/phase-two/lib/diaryentry.rb
+++ golden-square/phase-two/lib/diaryentry.rb
@@ -1,3 +1,16 @@
 class DiaryEntry
 
+    def initialize(title, contents)
+        @title = title
+        @contents = contents
+    end
+
+    def title 
+        return @title
+    end
+
+    def contents
+        return @contents
+    end
+    
 end
--- golden-square/phase-two/spec/diaryentry_spec.rb
+++ golden-square/phase-two/spec/diaryentry_spec.rb
@@ -1,5 +1,9 @@
 require 'diaryentry'
 
 describe DiaryEntry do
-
+    it 'should have a title and contents' do
+        diary_entry = DiaryEntry.new('my_title', 'my_contents')
+        expect(diary_entry.title).to eq 'my_title'
+        expect(diary_entry.contents).to eq 'my_contents'
+    end
 end
