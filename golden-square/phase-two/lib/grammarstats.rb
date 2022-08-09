4d99af1edaa6b02a90154082efe0822f0306fca0
zb-coder <zainab_baruud@hotmail.co.uk>
2022-08-09 13:30:58 +0100

    Adding tests and code for the check method

--- golden-square/phase-two/lib/grammarstats.rb
+++ golden-square/phase-two/lib/grammarstats.rb
@@ -3,7 +3,14 @@ class GrammarStats
       # ...
     end
   
-    def check(text) # text is a string
+    def check(text)
+        if text.empty? 
+            fail "sentence not provided"
+        elsif text[0] != text[0].upcase || !text.end_with?(".") && !text.end_with?("!")
+            return false
+        else
+            return true
+        end
       # Returns true or false depending on whether the text begins with a capital
       # letter and ends with a sentence-ending punctuation mark.
     end
---
+++ golden-square/phase-two/spec/grammarstats_spec.rb
@@ -0,0 +1,52 @@
+require 'grammarstats'
+
+describe GrammarStats do
+    describe '#check_text' do
+        it 'responds with one argument' do
+            grammar_stats = GrammarStats.new
+            expect(grammar_stats).to respond_to(:check).with(1).argument
+        end
+
+        context 'given empty string' do
+            it 'fails' do
+                grammar_stats = GrammarStats.new
+                expect{grammar_stats.check("")}.to raise_error "sentence not provided"
+            end
+        end
+
+        context 'given the text begins with a capital letter and ends with a sentence-ending punctuation mark' do
+            it 'returns true' do
+                grammar_stats = GrammarStats.new
+                expect(grammar_stats.check("The weather is nice.")).to eq true
+            end
+        end
+
+        context 'given the text begins with a capital letter and ends with a sentence-ending punctuation mark' do
+            it 'returns true' do
+                grammar_stats = GrammarStats.new
+                expect(grammar_stats.check("The weather is nice!")).to eq true
+            end
+        end
+
+        context 'given the text does not begin with a capital letter' do
+            it 'returns false' do
+                grammar_stats = GrammarStats.new
+                expect(grammar_stats.check("the weather is nice.")).to eq false
+            end
+        end
+
+        context 'given the text does not end with a sentence-ending punctuation mark' do
+            it 'returns false' do
+                grammar_stats = GrammarStats.new
+                expect(grammar_stats.check("The weather is nice")).to eq false
+            end
+        end
+
+        context 'given the text does not begin with a capital letter and does not end with a sentence-ending punctuation mark' do
+            it 'returns false' do
+                grammar_stats = GrammarStats.new
+                expect(grammar_stats.check("the weather is nice")).to eq false
+            end
+        end
+    end
+end
