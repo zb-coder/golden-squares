81a1e4d1191c7fd83a7733b58b2eb8cccae4e509
zb-coder <zainab_baruud@hotmail.co.uk>
2022-08-09 11:17:02 +0100

    adding challenge

---
+++ golden-square/phase-two/lib/grammarstats.rb
@@ -0,0 +1,15 @@
+class GrammarStats
+    def initialize
+      # ...
+    end
+  
+    def check(text) # text is a string
+      # Returns true or false depending on whether the text begins with a capital
+      # letter and ends with a sentence-ending punctuation mark.
+    end
+  
+    def percentage_good
+      # Returns as an integer the percentage of texts checked so far that passed
+      # the check defined in the `check` method. The number 55 represents 55%.
+    end
+  end
