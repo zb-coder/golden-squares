26913cef90c5eba931e2303c0c64ed11415d3d64
zb-coder <zainab_baruud@hotmail.co.uk>
2022-08-23 16:57:47 +0100

    adding a unit test for reading time in the diary spec

--- golden-square/phase-two/testing_multiple_classes/spec/diary_spec.rb
+++ golden-square/phase-two/testing_multiple_classes/spec/diary_spec.rb
@@ -7,10 +7,15 @@ describe Diary do
             diary = Diary.new
             expect(diary.all).to eq []
         end
-    end
 
         it "has a count of 0" do
             diary = Diary.new
             expect(diary.count_words).to eq 0
         end
+
+        it "has a reading time of 0" do
+            diary = Diary.new
+            expect(diary.reading_time(2)).to eq 0
+        end
+    end
 end
