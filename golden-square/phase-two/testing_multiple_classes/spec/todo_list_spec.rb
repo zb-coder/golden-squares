6c5757c49aaeb3e229639e16039a0aec4dfed261
zb-coder <zainab_baruud@hotmail.co.uk>
2022-08-24 18:41:05 +0100

    Adding another initial test for todolist file

--- golden-square/phase-two/testing_multiple_classes/spec/todo_list_spec.rb
+++ golden-square/phase-two/testing_multiple_classes/spec/todo_list_spec.rb
@@ -2,9 +2,14 @@ require 'todo_list.rb'
 
 describe TodoList do
     context 'initially' do
-        it 'is empty' do
+        it 'is empty incomplete array' do
             todo_list = TodoList.new
             expect(todo_list.incomplete).to eq []
+        end
+
+        it 'is empty complete array' do
+            todo_list = TodoList.new
+            expect(todo_list.complete).to eq []
         end
     end
 end
