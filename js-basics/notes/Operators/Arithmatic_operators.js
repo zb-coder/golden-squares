defdfc986ed40fbca21188818b4c6f2fc4861ec1
zb-coder <zainab_baruud@hotmail.co.uk>
2023-05-19 20:14:41 +0100

    javascript operators notes

---
+++ js-basics/notes/Operators/Arithmatic_operators.js
@@ -0,0 +1,20 @@
+let x = 10;
+let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y); This is remainder of division
+// console.log(x ** y); This is X to the power of y
+
+
+// Increment (++)
+console.log(++x); // When you put the increment operator before the x, the value of x will be incremented by 1 first and then we will see that on the console 1 + x = 11.
+console.log(x++); // When you put the increment operator after the x, you will see x on the console first and then the value of x will be incremented by 1 after. You will get 10 in the first console.log and then 11 in the second.
+console.log(x);
+
+// Decrement
+console.log(--x); // = 9 - because decremented first
+console.log(x--); // = 10 - because decremented after, so once you do console log of x after then it will be 9.
+console.log(x);
