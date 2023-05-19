f9e03a27a42ab73d3df252589455c53e9e2b0f11
zb-coder <zainab_baruud@hotmail.co.uk>
2023-05-19 20:58:12 +0100

    comparison operators

--- js-basics/notes/Operators/Assignment_operators.js
+++ js-basics/notes/Operators/Assignment_operators.js
@@ -3,4 +3,4 @@ let x = 10;
 x += 5; //This adds 5 to x. It is equivalent to x = x + 5;
 x *= 5; //This multiplies 5 to x. It is equivalent to x = x * 5;
 x /= 5; //This divides 5 to x. It is equivalent to x = x / 5;
-x %= 5 //This finds the reminder of x / 5. It is equivalent to x = x % 5;
+x %= 5; //This finds the reminder of x / 5. It is equivalent to x = x % 5;
---
+++ js-basics/notes/Operators/Comparison_operators.js
@@ -0,0 +1,14 @@
+
+let x = 1;
+
+// The result of an expression which includes a comparison operator (e.g <, >, ===) is a boolean
+// Relational Operators
+console.log( x < 0);
+console.log( x > 0);
+console.log( x <= 0);
+console.log( x >= 0);
+
+// Equality Operators
+console.log(x === 1); // checks if x is equal to 1
+console.log(x !== 1); // checks if x is not equal to 1
+
