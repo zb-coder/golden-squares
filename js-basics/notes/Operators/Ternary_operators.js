0c47e98e085dad26a8670082ccb0a3983d61551b
zb-coder <zainab_baruud@hotmail.co.uk>
2023-05-20 11:52:26 +0100

    ternary operators

---
+++ js-basics/notes/Operators/Ternary_operators.js
@@ -0,0 +1,8 @@
+// If a customer has more than 100 points,
+// they are a 'gold' customer, otherwise,
+// they are a 'silver' customer.
+
+let points = 110;
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log(type);
