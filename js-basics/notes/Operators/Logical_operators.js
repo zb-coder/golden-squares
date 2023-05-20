0d493799844adce8be992aea553ec42f59643b89
zb-coder <zainab_baruud@hotmail.co.uk>
2023-05-20 12:09:11 +0100

    logical operators

---
+++ js-basics/notes/Operators/Logical_operators.js
@@ -0,0 +1,23 @@
+
+// Logical AND (&&)
+// Returns TRUE if both operands are TRUE
+console.log(false && false);
+
+// An application for approving loans.
+// For the loan to be approved, the applicant needs to have,
+// a high income and a good credit score.
+let highIncome = true;
+let goodCreditScore = true;
+let = eligableForLoan = highIncome && goodCreditScore;
+
+console.log(eligableForLoan);
+
+//Logical OR (||)
+// Returns TRUE if one of the operands is TRUE
+console.log(true || false);
+
+//NOT (!)
+// Whatever we give it, it will give us the opposite.
+let applicationRefused = !eligableForLoan;
+console.log(eligableForLoan)
+
