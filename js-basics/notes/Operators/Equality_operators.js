6ac304cf8bb469becce89de4febde22563ba53ea
zb-coder <zainab_baruud@hotmail.co.uk>
2023-05-20 11:10:45 +0100

    Equality operator notes

---
+++ js-basics/notes/Operators/Equality_operators.js
@@ -0,0 +1,12 @@
+// Strict Equality (Type + Value)
+// This Operator ensures both numbers have the same type and have the same value.
+// The example below, on both sides of the operator we have two numbers, so their type as well as their value is the same.
+// console.log(1 === 1);
+
+
+// Lose Equality (Value)
+// This Operator compares the values only.
+// This Operator looks at what we have on the left side and then automatically converts the value on the right side to what is on the left.
+console.log(1 == 1);
+
+//Best practice is to use the strict equality operator.
