d6f4e6c4351631e0d230925bcf7fa1d394af9d50
zb-coder <zainab_baruud@hotmail.co.uk>
2023-05-25 13:05:10 +0100

    Infinite Loops

---
+++ js-basics/notes/Control_Flow/Infinite_loops.js
@@ -0,0 +1,11 @@
+//Infinite Loops
+
+let i = 0;
+// while (i < 5) {
+//     console.log(i);
+//     // i++
+// }
+
+// If you do not increment i, then i will always be less than
+// 5 and so it will keep looping.
+
