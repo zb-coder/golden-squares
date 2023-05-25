4b20615be3636ae99dfc40176283968c1d8bc69e
zb-coder <zainab_baruud@hotmail.co.uk>
2023-05-25 15:36:14 +0100

    solving max of numbers exercise using a function

---
+++ js-basics/notes/Control_Flow/Exercise_1.js
@@ -0,0 +1,12 @@
+// Max of two numbers
+
+// Write a function that takes two numbers and returns the
+// maximum of the two.
+
+function max(number1, number2) {
+    if (number1 > number2) return number1;
+    else return number2;
+}
+
+let number = max(44, 45);
+console.log(number);
