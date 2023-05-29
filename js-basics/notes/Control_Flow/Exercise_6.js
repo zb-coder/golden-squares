893b1067424b7b32e8a90fbbbef2996dbef2e5a5
zb-coder <zainab_baruud@hotmail.co.uk>
2023-05-29 14:52:25 +0100

    Adding second solution

--- js-basics/notes/Control_Flow/Exercise_6.js
+++ js-basics/notes/Control_Flow/Exercise_6.js
@@ -6,11 +6,28 @@
 // ''
 //NaN (not a number)
 
-function countTruthy(array) {
-    const onlyNumbers = (array.filter(Number))
-    console.log(onlyNumbers.length)
-}
+
+// Make a function that returns the number of truthy values in an array
+
+// Solution 1:
+
+// function countTruthy(array) {
+//     const onlyNumbers = (array.filter(Number))
+//     console.log(onlyNumbers.length)
+// }
 
 const array = [0, 1, 2, null, undefined, '']
 
-countTruthy(array)
+// countTruthy(array)
+
+// Solution 2:
+
+function countTruthy(array) {
+    let count = 0;
+    for (let value of array)
+        if(value) //This will only count truthy results
+            count ++;
+    return count;
+}
+
+console.log(countTruthy(array))
