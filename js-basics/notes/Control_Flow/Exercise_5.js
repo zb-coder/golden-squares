8e4c072f8113901afd85588ab40a0684ce3f8121
zb-coder <zainab_baruud@hotmail.co.uk>
2023-05-29 14:15:22 +0100

    Task complete, first attempt

---
+++ js-basics/notes/Control_Flow/Exercise_5.js
@@ -0,0 +1,16 @@
+// Make a function which has a list of numbers up to the limit
+// Next to every even number it should say 'EVEN'
+// Next to every odd number it should say 'Odd'
+// e.g. 0 Even
+// e.g. 1 Odd
+
+function showNumbers(limit){
+    for (let i = 0; i < limit; i ++) {
+        if (i % 2 === 0)
+            console.log (i, 'EVEN');
+        if (i % 2 !== 0)
+            console.log (i, 'ODD');
+    }
+}
+
+showNumbers(10);
