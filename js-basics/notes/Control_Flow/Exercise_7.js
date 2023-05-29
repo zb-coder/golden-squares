a39ae168999ae2270986b240ae4f50af263b020c
zb-coder <zainab_baruud@hotmail.co.uk>
2023-05-29 17:49:57 +0100

    refacturing

--- js-basics/notes/Control_Flow/Exercise_7.js
+++ js-basics/notes/Control_Flow/Exercise_7.js
@@ -4,14 +4,15 @@
 const movie = {
     title: 'a',
     releaseYear: 2018,
-    rating: '4.5',
+    rating: 4.5,
     director: 'b'
 };
 
 function showProperties(obj) {
-    for (key in obj)
+    for (let key in obj) {
         if (typeof obj[key] === 'string')
-            console.log(key, obj[key])
+            console.log(key, obj[key]);
+    }
 }
 
 showProperties(movie);
