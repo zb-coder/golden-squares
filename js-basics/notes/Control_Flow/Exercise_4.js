a0dd15c6556af49c99edbdf017bf99a382aeea0b
zb-coder <zainab_baruud@hotmail.co.uk>
2023-05-29 14:02:27 +0100

    refacturing

--- js-basics/notes/Control_Flow/Exercise_4.js
+++ js-basics/notes/Control_Flow/Exercise_4.js
@@ -11,15 +11,16 @@ function checkSpeed(speed) {
     const speedLimit = 70;
     const kmPerPoint = 5;
 
-    if (speed < speedLimit + kmPerPoint) 
-        console.log('Ok') 
-    else {
-        const points = Math.floor((speed - speedLimit) / kmPerPoint);
-        if (points >= 12)
-            console.log('License Suspended');
-        else 
-            console.log('Points :', points);
+    if (speed < speedLimit + kmPerPoint) {
+        console.log('Ok');
+        return; 
     }
+    
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points >= 12)
+        console.log('License Suspended');
+    else 
+        console.log('Points :', points);
 }
 
 checkSpeed(71);
