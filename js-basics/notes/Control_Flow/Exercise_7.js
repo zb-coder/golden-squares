bff079d13d23fec084672562dbe6f7f3bf17ad19
zb-coder <zainab_baruud@hotmail.co.uk>
2023-05-29 17:46:50 +0100

    returning string properties of an object

---
+++ js-basics/notes/Control_Flow/Exercise_7.js
@@ -0,0 +1,17 @@
+// A function that only displays the properties of the object
+// that are of type string
+
+const movie = {
+    title: 'a',
+    releaseYear: 2018,
+    rating: '4.5',
+    director: 'b'
+};
+
+function showProperties(obj) {
+    for (key in obj)
+        if (typeof obj[key] === 'string')
+            console.log(key, obj[key])
+}
+
+showProperties(movie);
