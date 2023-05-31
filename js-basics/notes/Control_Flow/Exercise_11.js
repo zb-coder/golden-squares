504ed8ef493fc99b5894d8486e1c16f6a250fef7
zb-coder <zainab_baruud@hotmail.co.uk>
2023-05-31 21:29:26 +0100

    adding another function as per the single responsibility principle

--- js-basics/notes/Control_Flow/Exercise_11.js
+++ js-basics/notes/Control_Flow/Exercise_11.js
@@ -1,17 +1,16 @@
 //  A function that shows all the prime numbers of the limit
 
 function showPrimes(limit){
-    for (let number = 2; number <= limit; number++) {
+    for (let number = 2; number <= limit; number++) 
+        if (isPrime(number)) console.log(number);
+}
 
-        let isPrime = true;
-        for (let factor = 2; factor < number; factor++) {
-        if (number % 2 === 0) {
-            isPrime = false;
-            break;
-            }
-        }
-        if (isPrime) console.log(number);
-    }
+function isPrime(number) {
+    for (let factor = 2; factor < number; factor++) 
+        if (number % 2 === 0) 
+            return false;
+        
+    return true;
 }
 
 showPrimes(20);
