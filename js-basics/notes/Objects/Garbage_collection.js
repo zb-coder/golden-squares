129f7594e6bb73f1819c849d9290f0ee498f1126
zb-coder <zainab_baruud@hotmail.co.uk>
2023-06-05 14:52:25 +0100

    garbage collector

---
+++ js-basics/notes/Objects/Garbage_collection.js
@@ -0,0 +1,7 @@
+// garbage collector
+
+// In Javascript we have something called a garbage collector
+// The job of the garbage collector is to find the variables or constants that are no longer used
+// and then deallocate the memory that was allocated to them earlier.
+
+// In Javascript memory allocation and deallocation happend automatically behind the scenes.
